9d3ea22595d37bba837da366334a2b5e48dddcef
Alex K <alex.workinfo97@gmail.com>
2022-06-24 16:17:38 +0300

    add test

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,20 +1,21 @@
 const { User } = require('../../models/users');
 const { Unauthorized } = require('http-errors');
 
-const login = async ({ body: { email, password } }, res, next) => {
+const login = async ({ body: { email, password } }, res) => {
   const userInDb = await User.findOne({ email });
   const passwordIsValid = await userInDb?.validPassword(password);
 
   if (!userInDb || !passwordIsValid) {
-    next(new Unauthorized('Email or password is wrong'));
+    throw new Unauthorized('Email or password is wrong');
   }
 
   const token = await userInDb.setToken(userInDb._id);
+
   const user = await User.findByIdAndUpdate(userInDb._id, userInDb).select(
     'email subscription -_id',
   );
 
-  res.json({ token, user });
+  return res.json({ code: 200, data: { token, user } });
 };
 
 module.exports = login;
